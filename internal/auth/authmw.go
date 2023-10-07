24b7536acdbb99df4a0acfc8369fe0e5d311c305
erups <e.rupshis@gmail.com>
2023-10-07 14:22:31 +0200

    split auth's module logic on files.

---
+++ internal/auth/authmw.go
@@ -0,0 +1,46 @@
+package auth
+
+import (
+	"net/http"
+	"strings"
+)
+
+func (c *Controller) AuthorizeUser(h http.Handler, userRoleRequirement int) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			c.log.Info("[%s:controller:AuthorizeUser] invalid request without authentication token", packageName)
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		token := strings.Split(authHeader, " ")
+		if len(token) != 2 || token[0] != "Bearer" {
+			c.log.Info("[%s:controller:AuthorizeUser] invalid invalid token", packageName)
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		userID := c.jwt.GetUserId(token[1])
+		userRole, err := c.usersStrg.GetUserRole(userID)
+		if err != nil {
+			c.log.Info("[%s:controller:AuthorizeUser] failed to search user in system: %v", packageName, err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if userRole == -1 {
+			c.log.Info("[%s:controller:AuthorizeUser] user is not registered in system", packageName)
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		if userRole < userRoleRequirement {
+			c.log.Info("[%s:controller:AuthorizeUser] user doesn't have permission to resource: %s", packageName, r.URL.Path)
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
---
+++ internal/auth/registerhandler.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"bytes"
+	"net/http"
+
+	"github.com/erupshis/bonusbridge/internal/auth/users/managers/ram"
+	"github.com/erupshis/bonusbridge/internal/helpers"
+)
+
+func (c *Controller) registerHandler(w http.ResponseWriter, r *http.Request) {
+	buf := bytes.Buffer{}
+	if _, err := buf.ReadFrom(r.Body); err != nil {
+		c.log.Info("[controller:registerHandler] failed to read request body: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer helpers.ExecuteWithLogError(r.Body.Close, c.log)
+
+	var user ram.User
+	if err := helpers.UnmarshalData(buf.Bytes(), &user); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		c.log.Info("[controller:registerHandler] bad new user input data")
+		return
+	}
+
+	userID, err := c.usersStrg.GetUserId(user.Login)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		c.log.Info("[controller:registerHandler] failed to check user in database")
+		return
+	}
+
+	if userID != -1 {
+		w.WriteHeader(http.StatusConflict)
+		c.log.Info("[controller:registerHandler] login already exists")
+		return
+	}
+
+	userID, err = c.usersStrg.AddUser(user.Login, user.Password)
+	if err != nil || userID == -1 {
+		w.WriteHeader(http.StatusInternalServerError)
+		c.log.Info("[controller:registerHandler] failed to add new user '%s'", user.Login)
+		return
+	}
+
+	token, err := c.jwt.BuildJWTString(userID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		c.log.Info("[controller:loginHandler] new token generation failed: %w", err)
+		return
+	}
+
+	w.Header().Set("Authorization", "Bearer "+token)
+	w.WriteHeader(http.StatusCreated)
+
+	c.log.Info("[controller:registerHandler] user '%s' registered successfully", user.Login)
+}
