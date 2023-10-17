287bb675325e8965f92e0de9f295d5d6cd12af7e
Edgar Rupshis <73851167+ERupshis@users.noreply.github.com>
2023-10-17 18:25:18 +0200

    review refactoring. small balance refactoring

--- internal/bonuses/storage/managers/postgresql/queries/bonuses/select.go
+++ internal/bonuses/storage/managers/postgresql/queries/bonuses/select.go
@@ -70,3 +70,68 @@ func createSelectDifByUserIDStmt(ctx context.Context, tx *sql.Tx, userID int64) 
 	}
 	return tx.PrepareContext(ctx, psqlSelect)
 }
+
+func SelectInOutSumByUserID(ctx context.Context, tx *sql.Tx, in bool, userID int64, log logger.BaseLogger) (float32, error) {
+	errMsg := fmt.Sprintf("select bonuses balance for userID '%d' in '%s'", userID, dbBonusesData.GetTableFullName(dbBonusesData.BonusesTable)) + ": %w"
+
+	stmt, err := createSelectSumByUserIDStmt(ctx, tx, in)
+	if err != nil {
+		return -1.0, fmt.Errorf(errMsg, err)
+	}
+	defer helpers.ExecuteWithLogError(stmt.Close, log)
+
+	var rows *sql.Rows
+	query := func(context context.Context) error {
+		rows, err = stmt.QueryContext(
+			context,
+			userID,
+			0,
+		)
+
+		if err == nil {
+			if rows.Err() != nil {
+				return fmt.Errorf(errMsg, rows.Err())
+			}
+		}
+
+		return err
+	}
+	err = retryer.RetryCallWithTimeoutErrorOnly(ctx, log, []int{1, 1, 3}, db.DatabaseErrorsToRetry, query)
+	if err != nil {
+		return -1.0, fmt.Errorf(errMsg, err)
+	}
+
+	defer helpers.ExecuteWithLogError(rows.Close, log)
+	var res sql.NullFloat64
+	for rows.Next() {
+		err = rows.Scan(
+			&res,
+		)
+		if err != nil {
+			return -1.0, fmt.Errorf("parse db result: %w", err)
+		}
+	}
+
+	return float32(res.Float64), nil
+}
+
+func createSelectSumByUserIDStmt(ctx context.Context, tx *sql.Tx, in bool) (*sql.Stmt, error) {
+	psql := sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
+
+	builder := psql.Select("SUM(count)").
+		From(dbBonusesData.GetTableFullName(dbBonusesData.BonusesTable)).
+		Where(sq.Eq{"user_id": 0})
+
+	if in {
+		builder = builder.Where(sq.GtOrEq{"count": 0})
+	} else {
+		builder = builder.Where(sq.LtOrEq{"count": 0})
+	}
+
+	psqlSelect, _, err := builder.ToSql()
+
+	if err != nil {
+		return nil, fmt.Errorf("squirrel sql select statement for '%s': %w", dbBonusesData.GetTableFullName(dbBonusesData.BonusesTable), err)
+	}
+	return tx.PrepareContext(ctx, psqlSelect)
+}
