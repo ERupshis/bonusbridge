283e57c84f7dc7f63f0f60f904442e5baf000de1
Edgar Rupshis <73851167+ERupshis@users.noreply.github.com>
2023-10-18 23:32:27 +0200

    tests. added mocks for orders interfaces

--- internal/orders/storage/interface.go
+++ internal/orders/storage/interface.go
@@ -6,6 +6,7 @@ import (
 	"github.com/erupshis/bonusbridge/internal/orders/data"
 )
 
+//go:generate mockgen -destination=../../../mocks/mock_BaseOrdersStorage.go -package=mocks github.com/erupshis/bonusbridge/internal/orders/storage BaseOrdersStorage
 type BaseOrdersStorage interface {
 	AddOrder(ctx context.Context, number string, userID int64) error
 	UpdateOrder(ctx context.Context, order *data.Order) error
--- internal/orders/storage/managers/interface.go
+++ internal/orders/storage/managers/interface.go
@@ -6,6 +6,7 @@ import (
 	"github.com/erupshis/bonusbridge/internal/orders/data"
 )
 
+//go:generate mockgen -destination=../../../../mocks/mock_BaseOrdersManager.go -package=mocks github.com/erupshis/bonusbridge/internal/orders/storage/managers BaseOrdersManager
 type BaseOrdersManager interface {
 	AddOrder(ctx context.Context, number string, userID int64) (int64, error)
 	UpdateOrder(ctx context.Context, order *data.Order) error
